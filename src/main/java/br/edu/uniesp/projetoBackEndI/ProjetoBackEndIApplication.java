3bf20e8fab1dd9524c5e8a84e94da73122d15533
Talita Hanna Cabral <talitalinden@gmail.com>
2023-09-08 15:09:56 -0300

    criacao do arquivo de configuracao

---
+++ src/main/java/br/edu/uniesp/projetoBackEndI/ProjetoBackEndIApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uniesp.projetoBackEndI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackEndIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackEndIApplication.class, args);
+	}
+
+}
